876e651324f55f911d55a71d249fb4a5d8d99da1
Sasial <44125644+sasial-dev@users.noreply.github.com>
2022-09-25 10:41:55 +1000

    refactor(lines2tree): rename to parseString

--- src/parse.ts
+++ src/parse.ts
@@ -130,7 +130,7 @@ const preprocessing = (lines:string[]):string[] => {
  * 
  * @param rawLines input array of string from the ICS file
  */
-export const lines2tree = (rawLines: string | string[]):TreeType => {
+export const parseString = (rawLines: string | string[]):TreeType => {
     if (!Array.isArray(rawLines)) rawLines = rawLines.split(NEW_LINE);
     const lines:string[] = preprocessing(rawLines)
     return process(lines)
